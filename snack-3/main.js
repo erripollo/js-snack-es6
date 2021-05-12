08980da9adab49c2ec2cce06172670555017eeab
erripollo <pollone.enrico@gmail.com>
2021-05-12 16:15:29 +0200

    Add new function version with Filter

--- snack-3/main.js
+++ snack-3/main.js
@@ -6,14 +6,14 @@ La funzione ritornerà un nuovo array con i valori che hanno la posizione compre
 */
 
 /**
- * ## Valori compresi tra "a" e "b"
+ * ## Valori compresi tra "a" e "b" (foreach mode)
  * La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
  * @param {Array} list array da cui estrapolare gli elementi
  * @param {Number} a a < b
  * @param {Number} b b < list.length
  * @returns {Array} array con valori compresi tra "a" e "b"
  */
-function betweenPositions (list, a, b) {
+function betweenPositionsWithForeach (list, a, b) {
     const extractList = [];
     list.forEach((element, i, list) => {
         if (i >= a && i <= b && b < list.length) {
@@ -23,6 +23,31 @@ function betweenPositions (list, a, b) {
     return extractList
 };
 
-var names = ['enrico', 'giulia', 'andrea', 'ettore', 'giulio', 'alessandra', 'valerio', 'davide', 'fabio'];
+/**
+ * ## Valori compresi tra "a" e "b" (filter mode)
+ * La funzione ritornerà un nuovo array con i valori che hanno la posizione compresa tra “a” e “b”
+ * @param {Array} list array da cui estrapolare gli elementi
+ * @param {Number} a a < b
+ * @param {Number} b b < list.length
+ * @returns {Array} array con valori compresi tra "a" e "b"
+ */
+function betweenPositionsWithFilter (list, a, b) {
+    const extractList = list.filter((element, i, list) => {
+        if (i >= a && i <= b && b < list.length) {
+            return true;
+        }
+        return false;
+    })
+    return extractList;
+};
 
-console.log(betweenPositions(names, 3, 6));
+
+const names = ['enrico', 'giulia', 'andrea', 'ettore', 'giulio', 'alessandra', 'valerio', 'davide', 'fabio'];
+
+const cities = ['torino', 'milano', 'bologna', 'londra', 'brcellona', 'monaco', 'parigi', 'porto', 'lisbona']
+
+console.log(betweenPositionsWithForeach(names, 3, 6));
+
+console.log(betweenPositionsWithFilter(cities, 4, 5));
+
+
